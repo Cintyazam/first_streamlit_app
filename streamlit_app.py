75c64566e9d000c7df693c400dc8956a871830f9
Cintyazam <123521443+Cintyazam@users.noreply.github.com>
2023-01-25 12:37:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,3 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
